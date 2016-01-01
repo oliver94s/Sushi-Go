374c57c4a5269c652fe00b2f8001901a8661ac79
Oliver Kim <oliver94s@yahoo.com>
2016-01-01 13:29:37 -0800

    More Cards Added
    I made a player class. I think what I need to do for the Play method in
    Cards is actually have a parameter for Player so that I could check the
    hand and the field that is played so far.

--- Cards.h
+++ Cards.h
@@ -2,17 +2,58 @@
 
 class Cards
 {
+private:
+    void setPoints(int value);
+
 protected:
     string name;
     int point;
 
-    setPoints(int value);
-
 public:
-    Cards(string name, int point);
+    Cards(string name);
     int getPoints() { return points; }
     string getName() { return name; }
     virtual void Play();
 
-}
 
+};
+
+class SalmonNigiri: public Cards
+{
+    void Play()
+    {
+        setPoints(2);
+    }
+};
+
+class SquidNigiri: public Cards
+{
+    void Play()
+    {
+        setPoints(3);
+    }
+};
+
+class EggNigiri: public Cards
+{
+    void Play()
+    {
+        setPoints(1);
+    }
+};
+
+class Wasabi: public Cards
+{
+    void Play()
+    {
+        //Next card if it is a nigiri x 3
+    }
+};
+
+class Pudding: public Cards
+{
+    void Play()
+    {
+        
+    }
+}
